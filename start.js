a632830a88c4f008949c5057e6a5231e6508931f
Dmitry Minkovsky <dminkovsky@gmail.com>
2013-10-25 11:21:31 -0400

    Improved start.js; added request console logging

--- start.js
+++ start.js
@@ -1,19 +1,17 @@
+// An HTTP server for your local development needs!
+
 var express = require('express')
   , server = express()
 
 require('freeport')(function(error, port) {
   if (!error) {
-    console.log('Running dminkovsky.github.io development server on http://localhost:%s/', port)
-    serve_files(server)
-      .listen(port)
+    server
+      .use(function(req, res, next) { console.log('%s %s', req.method, req.path); next() })
+      .use(express.static(__dirname))
+      .listen(port, function() { console.log('Running dminkovsky.github.io development server on http://localhost:%s/', port) })
     return
   }
   console.log('Failed to get a free port: %s', error)
-  console.log('Cannot run dminkovsky.github.io development server.', error)
+  console.log('Cannot run dminkovsky.github.io development server.')
   process.exit(1)
 })
-
-function serve_files(server) {
-  server.use(express.static(__dirname))
-  return server
-}
